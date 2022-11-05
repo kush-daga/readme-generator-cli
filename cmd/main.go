5de8e19f3e09b181317fe9ff4a62141fddff095a
Kush Daga <kushdaga@hotmail.com>
2022-11-05 18:15:07 +0530

    Add basic functions

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/kush-daga/readme-generator/pkg/prompts"
 )
@@ -15,7 +18,38 @@ func GetReadmeComponents() []string {
 		"FAQ",
 		"ScreenShots",
 		"Acknowledgments",
+		"License",
 	}
 	readmeComponents := prompts.MultiSelectPrompt("Please select all options you want to add in the Readme!", promptQs, survey.Required)
 	return readmeComponents
+}
+
+func AppendSectionToReadme(prompt string, options ...string) (ok bool, err error) {
+	fmt.Println("ADDING "+prompt+" TO README WITH OPTIONS", options)
+	return true, nil
+}
+
+func CreateReadme() (ok bool, err error) {
+	title := prompts.StringPrompt("Please enter the title of your project", false)
+	description := prompts.StringPrompt("Please enter the description of your project", true)
+
+	components := GetReadmeComponents()
+
+	for _, ele := range components {
+		if ele == "Title & Description" {
+			_, err := AppendSectionToReadme(ele, title, description)
+
+			if err != nil {
+				return false, errors.New("Error in writing Title and Desc to Readme File occurred: " + err.Error())
+			}
+		} else {
+			_, err := AppendSectionToReadme(ele)
+
+			if err != nil {
+				return false, errors.New("Error in writing " + ele + " to Readme File occurred: " + err.Error())
+			}
+		}
+	}
+
+	return true, nil
 }
--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/kush-daga/readme-generator/cmd"
 )
 
 func main() {
-	responses := cmd.GetReadmeComponents()
-	fmt.Printf("%s", responses)
+	cmd.CreateReadme()
 }
